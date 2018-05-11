01ac4bb30d149b25426676c25039b6b13c429e61
Paul Brownsmith <paul.brownsmith@waitrose.co.uk>
2018-05-11 19:25:01 +0100

    fleshing out tests

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,11 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+describe('egg wrapper component', () => {
+  // 
+});
+
+describe('egg component', () => {
+// 
+});
